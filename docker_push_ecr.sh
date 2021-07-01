e583479a9e361adbf236e04dae83c48cb27c0ece
Chris Delis <cedelis@uillinois.edu>
2021-07-01 13:08:27 -0500

    Add new out-of-office email filter to /usr/local/bin/rt-mailgate. For now, we will archive any email that have the following headers (eventually, we can simply exit w/out archiving, once we feel good about no false positives):
    auto-submitted: auto-generated
    X-Auto-Response-Suppress: All

--- docker_push_ecr.sh
+++ docker_push_ecr.sh
@@ -1,2 +1,2 @@
-docker tag cedelis/mailman:0.4 ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/mailman:0.4
-docker push ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/mailman:0.4
+docker tag cedelis/mailman:0.5 ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/mailman:0.5
+docker push ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/mailman:0.5
